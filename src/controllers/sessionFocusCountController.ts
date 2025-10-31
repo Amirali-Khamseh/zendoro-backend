4c925e38d19bedc78dcbf4c3004d61c7b721f211
Amir ali <amiralikhamseh01@gmail.com>
2025-10-31 21:39:45 +0100

    feat : add the getsessionFocusCount

--- src/controllers/sessionFocusCountController.ts
+++ src/controllers/sessionFocusCountController.ts
@@ -2,8 +2,9 @@ import { Response } from "express";
 import { AuthRequest } from "../middlewares/authMiddleware";
 import { db } from "../db";
 import { sessionFocusCounts } from "../db/schema";
+import { eq, desc } from "drizzle-orm";
 
-export async function sessionFocusCount(req: AuthRequest, res: Response) {
+export async function setSessionFocusCount(req: AuthRequest, res: Response) {
   try {
     const { sessionCount, date } = req.body;
     const userId = req.user?.userId;
@@ -33,6 +34,33 @@ export async function sessionFocusCount(req: AuthRequest, res: Response) {
     res.status(200).json({ message: "Session focus saved", data: inserted });
   } catch (error) {
     console.error("Error saving session focus count:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+export async function getSessionFocusCount(req: AuthRequest, res: Response) {
+  try {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    // Fetch the session count for the user
+    const sessionCountResult = await db
+      .select()
+      .from(sessionFocusCounts)
+      .where(eq(sessionFocusCounts.userId, userId))
+      .orderBy(desc(sessionFocusCounts.date))
+      .limit(1);
+
+    const sessionCount = sessionCountResult[0];
+
+    if (!sessionCount) {
+      return res.status(404).json({ error: "Session focus not found" });
+    }
+
+    res.status(200).json({ data: sessionCount });
+  } catch (error) {
+    console.error("Error fetching session focus count:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 }
--- src/routes/timerRoutes.ts
+++ src/routes/timerRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/authMiddleware";
 import { createMode, getModes } from "../controllers/modeController";
-import { sessionFocusCount } from "../controllers/sessionFocusCountController";
+import {
+  getSessionFocusCount,
+  setSessionFocusCount,
+} from "../controllers/sessionFocusCountController";
 
 const router = Router();
 
 router.post("/", authenticateToken, createMode);
 router.get("/", authenticateToken, getModes);
-router.post("/session-count", authenticateToken, sessionFocusCount);
+router.post("/session-count", authenticateToken, setSessionFocusCount);
+router.get("/session-count", authenticateToken, getSessionFocusCount);
 export default router;
